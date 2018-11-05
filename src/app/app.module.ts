0f2bb578cc2538c4516a5616d1c255cbc65d408c
Rob Ferguson <rob.ferguson@robferguson.org>
2018-11-05 18:38:10 +1100

    Updated the Individual component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
