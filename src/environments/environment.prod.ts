ab9631444b12a2ac82a6a5caff6ece68f86b846d
Rob Ferguson <rob.ferguson@robferguson.org>
2019-07-17 09:44:07 +1000

    Refactored the Auth and Auth Okta libraries

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,22 +13,22 @@ export const environment: Environment = {
   isDebugMode: false,
 
   firebase: {
-    apiKey: 'AIzaSyASxWQ8whc3CrA6EXYII07d_xa3q27RVDc',
-    authDomain: 'serendipity-f7626.firebaseapp.com',
-    databaseURL: 'https://serendipity-f7626.firebaseio.com',
-    projectId: 'serendipity-f7626',
-    storageBucket: 'serendipity-f7626.appspot.com',
-    messagingSenderId: '582668248299'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   },
 
   oidc: {
-    clientId: '0oatga8ebA4IkGg5U356',
-    issuer: 'https://dev-335467.okta.com/oauth2/default',
+    clientId: '',
+    issuer: '',
     redirectUri: 'http://localhost:4200/implicit/callback',
     scope: 'openid profile email phone address groups',
     responseType: 'id_token token',
     testing: {
-      disableHttpsCheck: true
+      disableHttpsCheck: false
     }
   },
 
