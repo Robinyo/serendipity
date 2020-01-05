5312cf45dc771b0e1e0f0e311926c05456a92555
Rob Ferguson <rob.ferguson@robferguson.org>
2020-01-05 12:33:01 +1100

    Updated proxy.conf.json

--- projects/flowable/src/lib/services/tasks/tasks.service.ts
+++ projects/flowable/src/lib/services/tasks/tasks.service.ts
@@ -40,7 +40,7 @@ export class TasksService extends CollectionService {
 
     // https://www.flowable.org/docs/userguide/index.html#_request_parameters
     const sort = 'createTime';
-    const order = 'desc';
+    const order = 'asc'; // 'desc'
     // const start = 0;
     // const size = 16;
 
