86c567972fd515f923aef0b8666be29f6b614201
Rob Ferguson <rob.ferguson@robferguson.org>
2021-12-05 15:03:01 +1100

    Merge commit

--- src/app/core/models/form-ids.ts
+++
@@ -1,3 +0,0 @@
-// export const PROFILE_GENERAL_INFORMATION_GROUP = 'profile-general-information-form';
-export const PROFILE_GENERAL_INFORMATION_GROUP = 'profile-general-information-form-read-only';
-export const PROFILE_ADDRESS_INFORMATION_GROUP = 'profile-address-information-form';
--- src/app/lazy-loading/party-lib-wrapper.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { PartyModule } from 'party';
-
-@NgModule({
-  imports: [ PartyModule ]
-})
-export class PartyLibWrapperModule {}
-
