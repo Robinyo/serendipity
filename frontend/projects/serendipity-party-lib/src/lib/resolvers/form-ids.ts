59b283d8c474e704bfb5d042cac6cd08bf28d598
Rob Ferguson <hey@rob-ferguson.me>
2026-08-09 17:20:00 +1000

    Updated the Contact component's Card View

--- frontend/projects/serendipity-party-lib/src/lib/resolvers/form-ids.ts
+++ frontend/projects/serendipity-party-lib/src/lib/resolvers/form-ids.ts
@@ -7,4 +7,4 @@ export const CONTACT_SUMMARY_TAB_FORM = 'contact-summary-tab-form';
 // export const CONTACT_WIZARD_GENERAL_INFORMATION_GROUP = 'contact-wizard-general-information-form';
 // export const CONTACT_WIZARD_ADDRESS_INFORMATION_GROUP = 'address-information-form';
 
-export const CONTACT_INFORMATION_FORM = 'contact-information-form';
+export const CONTACT_INFORMATION_FORM = 'contact-information-vertical-layout-form';
