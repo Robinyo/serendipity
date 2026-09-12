cc539326c670e32410b8a8e9b3690e1b1bb540bc
Rob Ferguson <hey@rob-ferguson.me>
2026-09-12 10:38:48 +1000

    Updated the Roadmap

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="Description will go into a meta tag in <head />">
+      description="Serendipity Docusaurus docs">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
