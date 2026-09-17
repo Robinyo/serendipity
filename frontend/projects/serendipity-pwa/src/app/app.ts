22d06ab8158979acd1ff17ba6319127cf53aa4d5
Rob Ferguson <hey@rob-ferguson.me>
2026-09-17 17:49:49 +1000

    Every PWA component sets ChangeDetectionStrategy.OnPush

--- frontend/projects/serendipity-pwa/src/app/app.ts
+++ frontend/projects/serendipity-pwa/src/app/app.ts
@@ -8,7 +8,7 @@ import { Layout } from './features/layout/layout';
     Layout
   ],
   templateUrl: './app.html',
-  changeDetection: ChangeDetectionStrategy.Eager,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './app.scss'
 })
 export class App {
